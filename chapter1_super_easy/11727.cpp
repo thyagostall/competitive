2fba3931847f483227abd18d4c9fbddd469cb341
thyagostall <thstall@gmail.com>
2017-02-16 22:24:31 -0200

    Modified problem 11727 to use best practices of C++11

--- chapter1_super_easy/11727.cpp
+++ chapter1_super_easy/11727.cpp
@@ -1,16 +1,18 @@
 #include <cstdio>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    int t, s[3];
+    int t;
+    vector<int> s(3, 0);
 
     scanf("%d", &t);
     for (int c = 1; c <= t; c++) {
         scanf("%d %d %d", &s[0], &s[1], &s[2]);
-        sort(s, s + 3);
+        sort(begin(s), end(s));
         printf("Case %d: %d\n", c, s[1]);
     }
 }
