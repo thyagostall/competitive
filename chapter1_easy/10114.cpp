a00192cce29b219988cd9d6bf36ed1865fa34129
thyagostall <thstall@gmail.com>
2017-02-18 00:32:55 -0200

    Problem 10114 - Accepted

--- chapter1_easy/10114.cpp
+++ chapter1_easy/10114.cpp
@@ -1,17 +1,36 @@
 #include <cstdio>
+#include <climits>
 
 using namespace std;
 
+const int INFINITY = INT_MAX;
+
 int main()
 {
-    double down, loan, depreciation_rate;
-    int month, months, records_qty;
+    int M, N, Mc, Mn, m;
+    float D, O, C, Dc, Dn, P;
 
-    while (scanf("%d %lf %lf %d", &months, &down, &loan, &records_qty) && months >= 0) {
-        printf("%d %lf %lf %d\n", months, down, loan, records_qty);
-        while (records_qty--) {
-            scanf("%d %lf", &month, &depreciation_rate);
-            printf("%d %lf\n", month, depreciation_rate);
+    while (scanf("%d %f %f %d", &M, &D, &O, &N) && M >= 0) {
+        C = O + D; 
+        P = O / M;
+
+        scanf("%d %f", &Mn, &Dn), N--;
+        for (m = 0; m <= M; m++) {
+            if (m == Mn) {
+                Mc = Mn, Dc = Dn;
+                Mn = INFINITY, Dn = 0;
+                if (N-- > 0) scanf("%d %f", &Mn, &Dn);
+            }
+
+            if (m > 0) O = O - P;
+            C = C * (1 - Dc);
+
+            if (O < C) break;
         }
+
+        if (m == 1)
+            printf("%d month\n", m);
+        else
+            printf("%d months\n", m);
     }
 }
