1d9e72a02f2756e61196fb019091845132202927
thyagostall <thstall@gmail.com>
2017-03-11 18:07:44 -0300

    Problem 11459 - Working

---
+++ chapter1_game_others_easier/uva11459.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+
+int main()
+{
+    int a, b, c, d, t, i, o, s;
+    int current_player;
+    int board[110];
+    int players[1000005];
+
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d %d %d", &a, &b, &c);
+        
+        for (i = 1; i <= 100; i++) {
+            board[i] = 0;
+        }
+        for (i = 1; i <= a; i++) {
+            players[i] = 1;
+        }
+        
+        while (b--) {
+            scanf("%d %d", &i, &o);
+            board[i] = o;
+        }
+
+        current_player = 1;
+        while (c--) {
+            scanf("%d", &d);
+            s = players[current_player];
+            if (s < 100) {
+                s += d;
+                s = s > 100 ? 100 : s;
+
+                if (board[s])
+                    s = board[s];
+            }
+
+            players[current_player] = s;
+            current_player++;
+            if (current_player > a) current_player = 1;
+        }
+
+        for (i = 1; i <= a; i++) {
+            printf("Position of player %d is %d.\n", i, players[i]);
+        }
+    }
+}
